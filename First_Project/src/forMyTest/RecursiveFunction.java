d98b6b42faefa9fc9c77fd4ce3363f6a33d13b4d
TJ-BU-708-P11 <TJ-BU-708-P11@DESKTOP-BAA6PC3>
2024-06-20 16:50:20 +0900

    RecursiveFunction - 재귀함수 만들어보기

--- First_Project/src/forMyTest/RecursiveFunction.java
+++ First_Project/src/forMyTest/RecursiveFunction.java
@@ -1,10 +1,31 @@
 package forMyTest;
 
+import java.util.Random;
+
 public class RecursiveFunction {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public static void main(String[] args) {
+		Random ran = new Random();
+		int numb = ran.nextInt(100) + 1;
+		System.out.println(numb);
+
+		solution(0, 100, numb, 0);
+
+	}
+
+	static void solution(int min, int max, int target, int count) {
+		
+		int mid = (max + min) / 2;
+		count++;
+		if (mid > target)
+			solution(min, mid, target, count);
+		else if (mid < target)
+			solution(mid, max, target, count);
+		else {
+			System.out.println("정답은 " + mid + " 입니다.");
+			System.out.println(count + "번 만에 찾았네요");
+		}
 	}
 
 }
