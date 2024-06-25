84a31f4b5ed99cbd4e04b92368aeefa0835a745d
TJ-BU-708-P11 <TJ-BU-708-P11@DESKTOP-BAA6PC3>
2024-06-25 18:08:03 +0900

    day7_Test3 2024.06.25 클래스 실습문제2

---
+++ First_Project/src/day7_Test3/Point3D.java
@@ -0,0 +1,27 @@
+package day7_Test3;
+
+public class Point3D extends Point {
+	private int z;
+
+	Point3D(int x, int y, int z) {
+		super(x, y);
+		this.z = z;
+	}
+
+	public String toString() {
+		return "(" + getX() + "," + getY() + "," + z +")의 점";
+	}
+
+	void moveUp() {
+		z++;
+	}
+
+	void moveDown() {
+		z--;
+	}
+
+	void move(int x, int y, int z) {
+		super.move(x, y);
+		this.z = z;
+	}
+}
---
+++ First_Project/src/day7_Test3/PositivePoint.java
@@ -0,0 +1,29 @@
+package day7_Test3;
+
+public class PositivePoint extends Point {
+
+	PositivePoint() {
+		super(0, 0);
+	}
+
+	PositivePoint(int x, int y) {
+		super(x, y);
+
+		int i = (x<0)? 0 : x;
+		int j = (y<0)? 0 : y;
+		super.move(i, j);
+
+	}
+
+	public String toString() {
+		return "(" + getX() + "," + getY() + ")의 점";
+	}
+
+	@Override
+	protected void move(int x, int y) {
+		if (x >= 0 && y >= 0)
+			super.move(x, y);
+
+	}
+
+}
