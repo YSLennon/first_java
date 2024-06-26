c851a2fff53627c7f88ab4629f6cad784cff24af
TJ-BU-708-P11 <TJ-BU-708-P11@DESKTOP-BAA6PC3>
2024-06-26 15:23:40 +0900

    day8_Test2 2024 06 26 자바 클래스 심화 문제 완료

--- First_Project/src/day8_Test2/Main.java
+++ First_Project/src/day8_Test2/Main.java
@@ -23,9 +23,11 @@ public class Main {
 				break;
 			}
 			case 2: // 조회
+				concert.check();
 				break;
 
 			case 3: // 예약 취소
+				concert.cancel();
 				break;
 
 			case 4: // 종료
@@ -60,13 +62,13 @@ class Concert {
 
 	}
 
-	void reserve() {
+	String checkNameAndPlace() {
 		System.out.print("좌석 구분 S(1), A(2), B(3)>>");
 		int grade = scan.nextInt();
 		scan.nextLine();
 		String position = "roof";
-		while(position == "roof") {
-			switch(grade) {
+		while (position == "roof") {
+			switch (grade) {
 			case 1:
 				System.out.print("S>>");
 				System.out.println(Arrays.toString(arrS));
@@ -82,38 +84,79 @@ class Concert {
 				System.out.println(Arrays.toString(arrB));
 				position = "arrB";
 				break;
-				default: 
-					System.out.println("잘못된 입력입니다.");
+			default:
+				System.out.println("잘못된 입력입니다.");
 			}
-			
+
 		}
 		System.out.print("이름>>");
 		String name = scan.nextLine();
+
+		return name + " " + position;
+	}
+
+	void reserve() {
+
+		String[] numberAndPlace = checkNameAndPlace().split(" ");
+
 		System.out.print("번호>>");
 		int numb = scan.nextInt();
-		switch(position) {
+		switch (numberAndPlace[1]) {
 		case "arrS":
-			arrS[numb-1] = name;
+			arrS[numb - 1] = numberAndPlace[0];
 			break;
 		case "arrA":
-			arrA[numb-1] = name;
+			arrA[numb - 1] = numberAndPlace[0];
 			break;
 		case "arrB":
-			arrB[numb-1] = name;
+			arrB[numb - 1] = numberAndPlace[0];
 			break;
-			default:		
+		default:
 		}
-		
 
 	}
-	
+
 	void check() {
-		
-	}
-	
-	void cancel() {
-		
-	}
-	
 
+		System.out.print("S>>");
+		System.out.println(Arrays.toString(arrS));
+		System.out.print("A>>");
+		System.out.println(Arrays.toString(arrA));
+		System.out.print(">>");
+		System.out.println(Arrays.toString(arrB));
+		System.out.println("<<<조회를 완료하였습니다.>>>");
+
+	}
+
+	void cancel() {
+		String[] numberAndPlace = checkNameAndPlace().split(" ");
+
+		end: switch (numberAndPlace[1]) {
+		case "arrS":
+			for (int i =0 ; i< arrS.length ; i++) {
+				if (arrS[i].equals(numberAndPlace[0])) {
+					arrS[i] = "---";
+					break end;
+				}
+			}
+			break;
+		case "arrA":
+			for (int i =0 ; i< arrA.length ; i++) {
+				if (arrA[i].equals(numberAndPlace[0])) {
+					arrA[i] = "---";
+					break end;
+				}
+			}
+		case "arrB":
+			for (int i =0 ; i< arrB.length ; i++) {
+				if (arrB[i].equals(numberAndPlace[0])) {
+					arrB[i] = "---";
+					break end;
+				}
+			}
+		default:
+
+		}
+
+	}
 }
