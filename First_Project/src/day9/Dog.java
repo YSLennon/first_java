e81a56803515f8c4d035c45081cdef429f394843
TJ-BU-708-P11 <TJ-BU-708-P11@DESKTOP-BAA6PC3>
2024-06-27 15:32:51 +0900

    day9 2024.06.27 상속 연습문제

---
+++ First_Project/src/day9/Dog.java
@@ -0,0 +1,24 @@
+package day9;
+
+public class Dog extends Animal {
+	private String category;
+
+	Dog() {
+		super("정보없음", 0);
+	}
+
+	Dog(String name, int age, String category) {
+		super(name, age);
+		this.category = category;
+	}
+
+	@Override
+	void sound() {
+		System.out.println("멍멍!");
+	}
+
+	String getBreed() {
+		return category;
+	}
+
+}
