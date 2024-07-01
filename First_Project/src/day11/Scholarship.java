a9aacfd5fe11dfa866218823dab490bd491c6d03
ysha0 <ysha0@YSPC>
2024-07-01 23:06:51 +0900

    day11 - Scholarship 2024.07.01 8교시 실습 문제 완료 -> 놓친 점 : HashMap의 생성을 반복문 외에 위치하여 동일한 HashMap에 모든 Pair가 들어갔었다. 명확하지 않은 변수 이름 지정으로 헷갈려서 조건식의 표현이 반대되었다.

--- First_Project/src/day11/Scholarship.java
+++ First_Project/src/day11/Scholarship.java
@@ -7,13 +7,16 @@ import java.util.Scanner;
 public class Scholarship {
 
 	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner(System.in);
-		HashMap<String, Object> hash = new HashMap<>();
 		ArrayList<HashMap<String, Object>> list = new ArrayList<>();
 
 
 		System.out.println("미래장학금관리시스템입니다.");
+		
 		for(int i=0; i<5 ; i++) {
+			HashMap<String, Object> hash = new HashMap<>();
+
 			System.out.print("이름과 학점>> ");
 			String[] userStr = scan.nextLine().split(" ");
 			hash.put("name",userStr[0]);
@@ -23,12 +26,12 @@ public class Scholarship {
 		}
 		System.out.print("장학생 선발 학점 기준 입력>> ");
 		System.out.println(list);
-		double point = Double.parseDouble(scan.nextLine());
+		double baseScore = Double.parseDouble(scan.nextLine());
 		String printString = "장학생 명단 : ";
 		
 		for(int i=0; i<list.size() ; i++) {
-			 double check = (double) list.get(i).get("point");
-			if(point >= check){
+			 double credit = (double) list.get(i).get("point");
+			if(baseScore <= credit){
 				printString += list.get(i).get("name") + " ";
 			}
 		}
