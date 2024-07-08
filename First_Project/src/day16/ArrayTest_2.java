828b12d09663197184ffb5c5cdc0f61866f6121f
YSPC <ysha0123@naver.com>
2024-07-08 23:47:40 +0900

    ArrayTest_2 -> 다양하게 다시 풀어봄

--- First_Project/src/day16/ArrayTest_2.java
+++ First_Project/src/day16/ArrayTest_2.java
@@ -1,34 +1,53 @@
 package day16;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
-// 실수한 부분 확인해보기 -> 두가지 방법으로 다 짜보자 
 // 2차원 배열 추가 문제
 
 
 public class ArrayTest_2 {
 
-	public static void main(String[] args) {
-		int[][] arr = new int[4][4];
+    public static void main(String[] args) {
+        makeArray(4);
 
-		int rowLength = arr.length - 1;
+    }
 
-		for (int row = 0; row < rowLength - 1; row++) {
-			int colLength = arr[row].length - 1;
+    private static void makeArray(int arrayLength) {
+        int[][] arr = new int[arrayLength][arrayLength];
+//		1번 보기
+//		for(int row = 0 ; row < arrayLength ; row++){
+//			for(int column =0 ; column < arrayLength -1 ; column++){
+//				if(row == arrayLength-1 ) continue;
+//				arr[row][column] = (int) (Math.random()*9+1);
+//				arr[row][arrayLength-1] += arr[row][column];
+//				arr[arrayLength-1][column] += arr[row][column];
+//				arr[arrayLength-1][arrayLength-1] += arr[row][column];
+//			}
+//			System.out.println(Arrays.toString(arr[row]));
+//		}
 
-			for (int col = 0; col < colLength - 1; col++) {
-				int number = (int) Math.random() * 10 + 1;
+//		2번 보기
+        for (int row = 0; row < arrayLength - 1; row++) {
+            for (int column = 0; column < arrayLength - 1; column++) {
+                arr[row][column] = (int) (Math.random() * 9 + 1);
+            }
+        }
+        int[] columnSumArray = new int[arrayLength];
 
-				arr[row][col] = number;
-				arr[row][colLength] += number;
-				arr[rowLength][col] += number;
-				arr[rowLength][colLength] += number;
+        for (int row = 0; row < arrayLength - 1; row++) {
+            int rowSum = 0;
+            for (int column = 0; column < arrayLength - 1; column++) {
+                rowSum += arr[row][column];
+                columnSumArray[column] += arr[row][column];
+            }
+            arr[row][arrayLength - 1] = rowSum;
+            columnSumArray[arrayLength - 1] += rowSum;
+            System.out.println(Arrays.toString(arr[row]));
+        }
+        arr[arrayLength - 1] = columnSumArray;
+        System.out.println(Arrays.toString(arr[arrayLength - 1]));
 
-			}
-			System.out.println(Arrays.toString(arr[row]));
-		}
 
-	}
+    }
 
 }
