0ccda4c003ee65e21ae128872b83358aaca61cba
TJ-BU-708-P11 <TJ-BU-708-P11@DESKTOP-BAA6PC3>
2024-07-10 15:21:36 +0900

    TEST

--- First_Project/src/Solution/Solution.java
+++ First_Project/src/Solution/Solution.java
@@ -5,10 +5,33 @@ import java.util.Scanner;
 
 public class Solution {
 
+	public Solution() {
+
+		a();
+	}
+	public void a() {
+		System.out.println("super");
+	}
+
 	public static void main(String[] args) {
 
-		String str = "select stu_name, stu_grade from student where stu_name = ' ' ";
+//		Solution a = new Solution();
+//		SubSolution b = new SubSolution();
+		Solution c = new SubSolution();
+//		String str = "select stu_name, stu_grade from student where stu_name = ' ' ";
 
+	}
 
+	
+}
+
+class SubSolution extends Solution {
+	@Override
+	public void a(){
+		System.out.println("sub");
+	}
+	public SubSolution() {
+		super.a();
+		
 	}
 }
