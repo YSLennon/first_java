4e6e0d452278655d6093b0c5847af9a2ae3d1191
TJ-BU-708-P11 <TJ-BU-708-P11@DESKTOP-BAA6PC3>
2024-07-15 14:33:19 +0900

    Day21- Test6_v2 개선 람다 추가 및 오버로딩을 통한 무조건 입력함수 개선

--- First_Project/src/javaTest0710/StringInputClass.java
+++ First_Project/src/javaTest0710/StringInputClass.java
@@ -4,27 +4,37 @@ import java.util.Scanner;
 
 public class StringInputClass {
 
-    //문자 입력, 조건 체크 후 재입력받는 함수
-    static void inputStr(InputWrapperClass strClass, String inputStr, CheckInputFlg flg) {
-        Scanner scan = new Scanner(System.in);
-        while (true) {
-            System.out.print(inputStr + " 입력하세요 : ");
-            strClass.str = scan.next();
-            if (flg.isTrue(strClass.str)) return;
+	CheckInputFlg defaultFlag = str -> {
+		return true;
+	};
 
-        }
-    }
+	// 문자 입력, 조건 체크 후 재입력받는 함수
+	static void inputStr(InputWrapperClass strClass, String inputStr, CheckInputFlg flg) {
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			System.out.print(inputStr + " 입력하세요 : ");
+			strClass.str = scan.next();
+			if (flg.isTrue(strClass.str))
+				return;
+		}
+	}
+	// 조건없이 입력만 받는 함수
+	static void inputStr(InputWrapperClass strClass, String inputStr) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print(inputStr + " 입력하세요 : ");
+		strClass.str = scan.next();
+	}
+	
 }
 
 // 입력받는 문자를 객체에 담아주는 Wrapper 클래스
 class InputWrapperClass {
-    String str;
+	String str;
 }
 
 // 문자 입력 시 조건을 체크하기 위한 인터페이스
 interface CheckInputFlg {
 
-    // isTrue 구현 시 틀린 값을 입력하면 안내 메세지 출력
-    boolean isTrue(String str);
+	// isTrue 구현 시 틀린 값을 입력하면 안내 메세지 출력
+	boolean isTrue(String str);
 }
-
